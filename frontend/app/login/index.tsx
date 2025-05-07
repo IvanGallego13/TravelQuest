08330455e834b8367576f460efe2a71d93b84746
PatxiBS <turronidus22@gmail.com>
2025-05-07 15:56:16 +0200

    Casi se me olvida un cambio del index

--- frontend/app/login/index.tsx
+++ frontend/app/login/index.tsx
@@ -70,7 +70,7 @@ export default function Login() {
       if (!res.ok) throw new Error(await res.text());
   
       const data = await res.json();
-      await login(data.token, data.user.id); // almacena el token recibido
+      await login(data.token, data.user.id); // almacena el token recibido y debe ser data.user.id
       router.replace("/login/localizacion");
     } catch (err) {
       Alert.alert("Error", "No se pudo iniciar sesión");
